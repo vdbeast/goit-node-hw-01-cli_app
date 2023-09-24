06d8fe103dcee70309c9659a08452ecb900725e5
vdbeast <122833284+vdbeast@users.noreply.github.com>
2023-09-24 20:12:11 +0300

    +

---
+++ README.md
@@ -0,0 +1,7 @@
+https://monosnap.com/file/6Cw0frFRDxHnBukSKa00zA4beqrswz - # Отримуємо і виводимо весь список контактів у вигляді таблиці (console.table)
+
+https://monosnap.com/file/HRSadiC4u5U5Mj6PVjmEYCYWrdmxoT - # Отримуємо контакт по id і виводимо у консоль об'єкт контакту або null, якщо контакту з таким id не існує.
+
+https://monosnap.com/file/Y4nqYbx9zfWC0rusZUMilizrzkynoX - # Додаємо контакт та виводимо в консоль об'єкт новоствореного контакту
+
+https://monosnap.com/file/i9rd8F4vPSDZdw9ySszzm8wITxF7Qb - # Видаляємо контакт та виводимо в консоль об'єкт видаленого контакту або null, якщо контакту з таким id не існує.
--- index.js
+++ index.js
@@ -18,22 +18,22 @@ async function invokeAction({ action, id, name, email, phone }) {
     switch (action) {
       case 'list':
         const contactsList = await contacts.listContacts();
-        console.log(contactsList);
+        console.table(contactsList);
         break;
 
       case 'get':
         const oneContact = await contacts.getContactById(id);
-        console.log(oneContact);
+        console.table(oneContact);
         break;
 
       case 'add':
-        const newContact = await contacts.addContact({ name, email, phone });
-        console.log(newContact);
+        const newContact = await contacts.addContact( name, email, phone );
+        console.table(newContact);
         break;
 
       case 'remove':
         const deleteContact = await contacts.removeContact(id);
-        console.log(deleteContact);
+        console.table(deleteContact);
         break;
 
       default:
